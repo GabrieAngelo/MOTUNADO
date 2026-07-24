b4310597f9b7a6bdf690874f6e1e44ce3717fcdc
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-07-24 11:27:42 -0300

    new

--- script.js
+++ script.js
@@ -9,45 +9,5 @@ function fecharmenu() {
 
 }
 
-
-const xre190 = document.querySelector("#xre190");
-
-xre190.addEventListener("mouseenter", () => {
-    const banner = document.querySelector(".banner");
-    banner.classList.remove("teste");
-    banner.classList.add("xre190");
-});
-xre190.addEventListener("mouseleave", () => {
-    const banner = document.querySelector(".banner");
-    banner.classList.add("teste");
-    banner.classList.remove("xre190");
-
-});
-
-const xre300 = document.querySelector("#xre300")
-
-xre300.addEventListener("mouseenter", () => {
-    const banner = document.querySelector(".banner")
-    banner.classList.remove("teste");
-    banner.classList.add("xre300");
-
-});
-xre300.addEventListener("mouseleave", () => {
-    const banner = document.querySelector(".banner")
-    banner.classList.remove("xre300");
-    banner.classList.add("teste");
-
-});
-
-const cb500x = document.querySelector("#cb500x")
-
-cb500x.addEventListener("mouseenter", () => {
-    const banner = document.querySelector(".banner")
-    banner.classList.remove("teste");
-    banner.classList.add("cb500x");
-});
-cb500x.addEventListener("mouseleave", () => {
-    const banner = document.querySelector(".banner")
-    banner.classList.remove("cb500x");
-    banner.classList.add("teste");
-});
+const modelo = document.querySelector("#modelo")
+modelo.addEventListener("click", abrirmenu);
