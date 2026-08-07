c3778f5c37d68215b8e5fb76add943e01b44e5f9
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-08-07 09:50:35 -0300

    ab1on

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function fecharmenu() {
 
 const modelo = document.querySelector("#modelo")
 modelo.addEventListener("click", abrirmenu);
+
+
+const ab1 = document.querySelector(".ab1");
+
+ab1.addEventListener("mouseenter", ab1on);
+
+function ab1on() {
+
+    ab1.style.backgroundColor = "#f2f2f2";
+}
+ab1.addEventListener("mouseleave", ab1off);
+
+function ab1off() {
+
+    ab1.style.backgroundColor = "rgb(0, 255, 34)";
+
+}
