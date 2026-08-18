3f089f0c5423ecfb6d3e67ddb58dc62ca5314336
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-08-18 16:40:33 -0300

    1808

--- script.js
+++ script.js
@@ -15,17 +15,34 @@ modelo.addEventListener("click", abrirmenu);
 
 
 
-
 const ab = document.querySelectorAll(".ab");
 
 ab.forEach(function(ab) {
+
+    const ab1foto = ab.querySelector(".ab1foto");
+    const textbox = ab.querySelector(".textbox");
+    const des = ab.querySelector(".des");
+    const valor = ab.querySelector(".valor");
+
     ab.addEventListener("mouseenter", function() {
         ab.classList.add("borda1");
-    });
-});
+        ab1foto.classList.add("ab1on");
+        textbox.classList.add("vermelho");
 
-ab.forEach(function(ab) {
+        valor.classList.add("textobranco");
+        des.classList.add("textobranco");
+
+
+    });
+
     ab.addEventListener("mouseleave", function() {
         ab.classList.remove("borda1");
+        ab1foto.classList.remove("ab1on");
+        textbox.classList.remove("vermelho");
+
+        valor.classList.remove("textobranco");
+        des.classList.remove("textobranco");
+
+
     });
 });
